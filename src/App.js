706685b8748e24d0b8fea9c1ab4d953c5cd66292
Inupedia <xenthorpis2016@gmail.com>
2022-03-23 00:49:53 -0600

    add antd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Button, message } from "antd";
 
-function App() {
+export default function App() {
+  const success = () => {
+    message.success("This is a success message");
+  };
+
+  const error = () => {
+    message.error("This is an error message");
+  };
+
+  const warning = () => {
+    message.warning("This is a warning message");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button type="primary" onClick={success}>
+        Primary
+      </Button>
     </div>
   );
 }
-
-export default App;
