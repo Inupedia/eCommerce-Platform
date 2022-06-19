42d01b6a3433bee9d91941c86975f9bfccf21ac0
Inupedia <xenthorpis2016@gmail.com>
2022-06-19 02:39:00 -0600

    category layout

--- src/pages/category/category.jsx
+++ src/pages/category/category.jsx
@@ -1,5 +1,105 @@
 import React from "react";
-
+import {
+    Card,
+    Table,
+    Button
+} from 'antd'
+import { PlusOutlined } from '@ant-design/icons';
+import LinkButton from "../../components/linkButton";
 export default function Category() {
-    return <div>category</div>
+    const title = "Class By Department"
+    const addButton = (
+        <Button>
+            <PlusOutlined />
+            Add
+        </Button>
+    )
+
+    const dataSource = [
+        {
+            "parentId": "0",
+            "_id": "1",
+            "name": "Food & Grocery",
+            "__v": 0
+        },
+        {
+            "parentId": "0",
+            "_id": "2",
+            "name": "Clothing, Shoe, Jewelry & Watches",
+            "__v": 0
+        },
+        {
+            "parentId": "0",
+            "_id": "3",
+            "name": "Pharmacy",
+            "__v": 0
+        },
+        {
+            "parentId": "0",
+            "_id": "4",
+            "name": "Pet Supplies",
+            "__v": 0
+        },
+        {
+            "parentId": "0",
+            "_id": "5",
+            "name": "Electronics",
+            "__v": 0
+        },
+        {
+            "parentId": "0",
+            "_id": "6",
+            "name": "Toys, Kids & Baby",
+            "__v": 0
+        },
+        {
+            "parentId": "0",
+            "_id": "7",
+            "name": "Music, Movies & Games",
+            "__v": 0
+        },
+        {
+            "parentId": "0",
+            "_id": "8",
+            "name": "Books",
+            "__v": 0
+        },
+        {
+            "parentId": "0",
+            "_id": "9",
+            "name": "Beauty & Health",
+            "__v": 0
+        }
+    ];
+
+    const columns = [
+        {
+            title: 'Class Name',
+            dataIndex: 'name',
+            key: 'name',
+        },
+        {
+            title: 'Operation',
+            render: () => (
+                <span>
+                    <LinkButton>Modify</LinkButton>
+                    <LinkButton>Check</LinkButton>
+                </span>
+            )
+        }
+    ];
+
+    return <div>
+        <Card
+            title={title}
+            extra={addButton}
+        >
+            <Table
+                bordered
+                rowKey="_id"
+                dataSource={dataSource}
+                columns={columns}
+            />
+        </Card>
+    </div>
 }
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
+import './home.less'
 
 export default function Home() {
-    return <div>home</div>
+    return <div className="home">Welcome to Chewy administration system</div>
 }
